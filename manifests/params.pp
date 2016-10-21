c51943f342daf4d4822286e30b9c11f71a19d353
Dennis Hoppe <github@debian-solutions.de>
2016-10-21 15:20:04 +0200

    Fix several lint issues

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@
 #
 class visualstudio::params {
   $temp_dir = 'C:\Windows\Temp'
-  
+
   $component_list = [
     'WebTools', 'OfficeTools', 'SharepointTools', 'LightSwitch', 'SilverLight_Developer_Kit',
     'SQL','VC_MFC_Libraries','Blend','BlissHidden','HelpHidden', 'LocalDBHidden', 'NetFX4Hidden',
@@ -13,12 +13,12 @@ class visualstudio::params {
     'ProfilerHidden','ReportingHidden','RIAHidden','SDKTools3Hidden','SDKTools4Hidden','Silverlight5DRTHidden',
     'SQLCEHidden','SQLCEToolsHidden','SQLCLRTypesHidden','SQLDACHidden','SQLDbProviderHidden','SQLDOMHidden',
     'SQLSharedManagementObjectsHidden','TSQLHidden','VCCompilerHidden','VCCoreHidden','VCDebugHidden',
-    'VCDesigntimeHidden','VCExtendedHidden','WCFDataServicesHidden','WinJSHidden','WinSDKHidden'
+    'VCDesigntimeHidden','VCExtendedHidden','WCFDataServicesHidden','WinJSHidden','WinSDKHidden',
   ]
 
   $vs_versions = {
     '2012' => {
-      'editions' => ['Ultimate','Premium','Test Professional','Professional']
-    }
+      'editions' => ['Ultimate','Premium','Test Professional','Professional'],
+    },
   }
 }
